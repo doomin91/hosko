b9b127e7ebfceff168b919cb6b65895d229c15cc
kimdoomin <kimduumin@withnetworks.com>
2021-04-20 20:44:55 +0900

    GROUP 초기 작업

---
+++ application/views/admin/board/group-list.php
@@ -0,0 +1,59 @@
+	<!-- Wrap all page content here -->
+	<div id="wrap">
+
+		<!-- Make page fluid -->
+		<div class="row">
+
+
+			<!-- Page content -->
+			<div id="content" class="col-md-12">
+
+			</div>
+			<!-- /page header -->
+
+			<!-- content main container -->
+			<div class="main">
+
+				<!-- row -->
+				<div class="row">
+
+					<!-- col 6 -->
+					<div class="col-md-12">
+						<!-- tile -->
+
+						<section class="tile color transparent-black">
+
+							<!-- tile body -->
+							<div class="tile-body">
+								<table class="table table-bordered">
+									<tbody>
+										<tr>
+											<th>번호</th>
+											<th>분류명</th>
+											<th>기능</th>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+							<!-- /tile body -->
+
+						</section>
+
+
+					</div>
+					<!-- /col 12 -->
+
+				</div>
+				<!-- /row -->
+
+			</div>
+			<!-- /content container -->
+
+		</div>
+		<!-- Page content end -->
+
+	</div>
+	<!-- Make page fluid-->
+
+	</div>
+	<!-- Wrap all page content end -->
